5a45f32fc9629ffcdfd5fd92dbc8e1f1bb5695c3
Ariel Berkovich <Arielberkovich1@gmail.com>
2019-03-13 17:17:00 +0200

    Added a class to help get the full path for a resource. Resources should now be placed in the same directory as the project runnable file inside "resources" folder

---
+++ GUI/GUI_utils.h
@@ -0,0 +1,16 @@
+#ifndef GUI_UTILS
+#define GUI_UTILS
+#include <QCoreApplication>
+#include <String>
+
+class GuiUtils
+{
+	public:
+		static std::string getFullPath(std::string fileName) {
+			std::string fileFullPath = QCoreApplication::applicationDirPath().toStdString();
+			fileFullPath.append("/resources/");
+			fileFullPath.append(fileName);
+			return fileFullPath;
+		}
+};
+#endif // GUI_UTILS
