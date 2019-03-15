20c7bd55114fdf76810df574ff866f49541fd3b2
Ariel Berkovich <Arielberkovich1@gmail.com>
2019-03-15 15:51:42 +0200

    Corrected breeder so it now compiles

--- GA/Breeder.h
+++ GA/Breeder.h
@@ -1,14 +1,20 @@
 #pragma once
 #include <vector>
+#include <numeric>
+#include <set>
+#include <algorithm>
+#include <limits>
+#include "GA_Random.h"
 template <class Creature>
 class Breeder
 {
 public:
-	static std::vector<Creature> generateNextGeneration(std::vector<Creature>&currentPopulation);
+	static std::vector<Creature> generateNextGeneration(std::vector<Creature>& currentPopulation);
 private:
-	static void normalizePopulationFittnesses(std::vector<Creature>&currentPopulation);
-	static std::discrete_distribution<int> createSelectionRoulette(std::vector<Creature>&currentPopulation, std::vector<int>& fitness);
-	static void createNextGenerationPopulation(std::vector<Creature>&currentPopulation, std::discrete_distribution<int>& roulette,
-											   std::vector<Creature>&newPopulation);
+	static void normalizePopulationFittnesses(std::vector<Creature>& currentPopulation);
+	static void createNextGenerationPopulation(std::vector<Creature>& currentPopulation, std::discrete_distribution<int>& roulette,
+											   std::vector<Creature>& newPopulation);
+
+	static std::discrete_distribution<int> createSelectionRoulette(std::vector<Creature>& currentPopulation, std::vector<int>& fitness);
 
 };
