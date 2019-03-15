771666c9ade3dc6491d63068d582b0d2e21dfec2
Ariel Berkovich <Arielberkovich1@gmail.com>
2019-03-15 14:56:00 +0200

    Refactored breeder's generateNewPopulation function

--- GA/Breeder.cpp
+++ GA/Breeder.cpp
@@ -4,60 +4,84 @@
 #include <algorithm>
 #include <limits>
 #include "GA_Random.h"
+#include <algorithm>    
 
 //------------------------------------------------------------------------------------------
 template <class Creature>
 std::vector<Creature> Breeder<Creature>::generateNextGeneration(std::vector<Creature>&currentPopulation)
 {
-    float mutationChance = 0.2;
-    int min = std::numeric_limits<int>::max();
-    for (int i = 0; i < currentPopulation.size(); i++)
-    {
-        if (currentPopulation[i].getFitness() < min)
-            min = currentPopulation[i].getFitness();
-    }
-    
-    if (min < 0)
-    {
-        for (int i = 0; i < currentPopulation.size(); i++)
-            currentPopulation[i].setFitness(currentPopulation[i].getFitness() - (min + 1));
-    }
-    
-	// set the probabilities for each Creature to be chosen
 	std::vector<int> fitness;
+	std::discrete_distribution<int> roulette;
+	
+	//create roulette wheel to choose parents
+	roulette = createSelectionRoulette(currentPopulation,fitness);	//Note: the Probabilities for each Creature to be chosen in the
+																	//roulette are chsoen in corelation with his fittness
+
+	//create next generation population
+	std::vector<Creature> newPopulation;
+	createNextGenerationPopulation(currentPopulation, roulette, newPopulation);
+
+	       
+	return newPopulation;
+}
+//------------------------------------------------------------------------------------
+template <class Creature>
+std::discrete_distribution<int> Breeder<Creature>::createSelectionRoulette(std::vector<Creature>&currentPopulation, std::vector<int>& fitness)
+{
+	//normalize populationFittnesses
+	normalizePopulationFittnesses(currentPopulation);
+
 	fitness.reserve(currentPopulation.size());
-	for (unsigned int i = 0; i < currentPopulation.size(); i++)
+	for (Creature& creature : currentPopulation)
 	{
 		fitness.push_back(currentPopulation[i].getFitness());
 	}
 	// roulette will choose random pop based on the probabilities vector 
-	std::discrete_distribution<int> roulette(fitness.begin(), fitness.end());
+	return std::discrete_distribution<int> roulette(fitness.begin(), fitness.end());
+}
+//------------------------------------------------------------------------------------
+template <class Creature>
+void Breeder<Creature>::normalizePopulationFittnesses(std::vector<Creature>&currentPopulation)
+{
+	int min = std::numeric_limits<int>::max();
+	//get minimum fittness in population
+	for (Creature& creature : currentPopulation)  {  min = std::min(min, creature.getFitness()); }
 	
-	std::vector<Creature> newPopulation;
+	if (min < 0)  
+	{
+		//normalize fittness 
+		for (Creature& creature : currentPopulation)
+		{
+			creature.setFitness(creature.getFitness() - (min + 1));
+		}
+	}
+}
+//------------------------------------------------------------------------------------
+template <class Creature>
+void Breeder<Creature>::createNextGenerationPopulation(std::vector<Creature>&currentPopulation,
+													   std::discrete_distribution<int>& roulette,
+													   std::vector<Creature>&newPopulation){
+	float mutationChance = 0.2;
 	int currentPopulationSize = currentPopulation.size();
-	newPopulation.reserve(currentPopulationSize*2);
-	
+	newPopulation.reserve(currentPopulationSize * 2);
+
 	for (int i = 0; i < currentPopulationSize; i++)
 	{
 		int parent1Index = roulette(Random::default_engine.getGenerator());
 		int parent2Index;
-		do { parent2Index = roulette(Random::default_engine.getGenerator()); }
-		while (parent2Index == parent1Index);
-		            
-        currentPopulation[parent1Index].crossover(currentPopulation[parent2Index], newPopulation);
-        newPopulation[newPopulation.size() - 1].mutate(mutationChance);
-        newPopulation[newPopulation.size() - 1].calculateFittness();
-        newPopulation[newPopulation.size() - 2].mutate(mutationChance);
-        newPopulation[newPopulation.size() - 2].calculateFittness();
+		do { parent2Index = roulette(Random::default_engine.getGenerator()); } while (parent2Index == parent1Index);
+
+		currentPopulation[parent1Index].crossover(currentPopulation[parent2Index], newPopulation);
+		newPopulation[newPopulation.size() - 1].mutate(mutationChance);
+		newPopulation[newPopulation.size() - 1].calculateFittness();
+		newPopulation[newPopulation.size() - 2].mutate(mutationChance);
+		newPopulation[newPopulation.size() - 2].calculateFittness();
 	}
-	
-    std::sort(currentPopulation.begin(),currentPopulation.end(), [](const Creature& a, const Creature& b){return (a.getFitness() > b.getFitness());});
+
+	std::sort(currentPopulation.begin(), currentPopulation.end(), [](const Creature& a, const Creature& b) {return (a.getFitness() > b.getFitness()); });
 	for (int i = 0; i < 5; i++)
 		newPopulation.push_back(currentPopulation[i]);
-	        
-	return newPopulation;
 }
-
 //------------------------------------------------------------------------------------
 // Force instantiation of BinaryCreature and PermutationCreature
 #include "Encoding/BinaryCreature.h"
