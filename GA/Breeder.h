18b4f48cb13c8a46f14aa9a2f8f6bec77c7b4b2a
Ariel Berkovich <Arielberkovich1@gmail.com>
2019-03-17 01:39:32 +0200

    Started working on  a "fair" selection roulette which should prevent a siutation in which the weak creatures dont have a chance of getting choosed at all. This fair roulette should make the probabilities graph linear and give weak creatures a bit more chance to get chosen while still letting strong creatures have a decent advantage over them.

--- GA/Breeder.h
+++ GA/Breeder.h
@@ -11,6 +11,7 @@ public:
 	static std::vector<Creature> generateNextGeneration(std::vector<Creature>& currentPopulation);
 private:
 	static void normalizePopulationFittnesses(std::vector<Creature>& currentPopulation);
+	static std::discrete_distribution<int> createFairFittnesesForRouletteSelection(std::vector<Creature>& currentPopulation);
 	static std::discrete_distribution<int> createSelectionRoulette(std::vector<Creature>& currentPopulation);
 
 };
