9a0e3269f99e6f728f3a0a06b6526ed637add364
XmakerenX <XmakeerenX@gmail.com>
2019-06-21 15:56:20 +0300

    Added icon to windows executables

--- resources/resource.h
+++ resources/resource.h
@@ -2,7 +2,7 @@
 #define _RESOURCE_H
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by 2D_Bin_Packing_Solver.rc
+// Used by pirate_packing.rc
 #define IDI_SMALL 103
 
 #endif // _RESOURCE_H
